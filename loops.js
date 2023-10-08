11aeb40070e71d7ab03a6e5640abe2daa27168a0
Rayaan Ahmed <ryacqub11@gmail.com>
2023-10-08 14:49:22 +0100

    first commit

---
+++ loops.js
@@ -0,0 +1,39 @@
+//for (let i =0; i <10; i++)
+//console.log('Rayaan')
+
+
+
+
+
+
+
+// while (counter <20){
+//     console.log("Ahmed")
+//     counter++;
+// }
+
+
+
+//while (counter < 20) {
+    //console.log("ahmed")
+    //counter++;
+//}
+
+
+// while (counter < 20);
+// console.log('hey')
+// counter++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
